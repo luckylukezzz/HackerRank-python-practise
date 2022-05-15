a8bf679e0475a5c56f31216bd8cfa246c5972d67
luckylukezzz <50476499+luckylukezzz@users.noreply.github.com>
2022-05-15 18:41:15 +0800

    word order
    https://www.hackerrank.com/challenges/word-order/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

---
+++ word order.py
@@ -0,0 +1,8 @@
+from collections import OrderedDict
+dct = OrderedDict()
+n=int(input())
+for i in range(n):
+  x=input()
+  dct[x]=(dct[x]+1 if x in dct else 1 )
+print(len(dct.keys()))
+print(*dct.values())
