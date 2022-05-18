70d6a029e882b404f6d28a188425bc50968ec332
luckylukezzz <50476499+luckylukezzz@users.noreply.github.com>
2022-05-18 22:34:52 +0800

    union intersection remove discard
    https://www.hackerrank.com/challenges/symmetric-difference/problem?isFullScreen=true&h_r=next-challenge&h_v=zen

---
+++ sets union interse.py
@@ -0,0 +1,11 @@
+M=int(input())
+a=set(map(int,input().split()))
+N=int(input())
+b=set(map(int,input().split()))
+x=a.intersection(b)
+for i in x:
+    a.remove(i)
+    b.remove(i)
+y=sorted(a.union(b))
+for i in y:
+    print(i)
