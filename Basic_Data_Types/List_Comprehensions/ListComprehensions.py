345564078aa33cb3a879720673e36cb0cf99a7d4
luckylukezzz <patalee2001@gmail.com>
2023-02-16 00:04:30 +0530

    added

---
+++ Basic_Data_Types/List_Comprehensions/ListComprehensions.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    l1=[]
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i+j+k !=n :
+                    l1.append([i,j,k])
+    print(l1)
