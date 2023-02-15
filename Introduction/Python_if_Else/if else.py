273664f0873ac9e5ea7b777da709148f61bf1de7
luckylukezzz <patalee2001@gmail.com>
2023-02-15 23:18:57 +0530

    added comments

--- Introduction/Python_if_Else/if else.py
+++ Introduction/Python_if_Else/if else.py
@@ -1,3 +1,20 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+# Input Format
+
+# A single line containing a positive integer, .
+
+# Output Format
+
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+
+
 import math
 import os
 import random
