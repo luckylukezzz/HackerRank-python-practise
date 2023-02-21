da460e3832ac0fedc70e8c778adec19742398e31
luckylukezzz <patalee2001@gmail.com>
2023-02-21 19:31:33 +0530

    modified

--- 04-Sets/13-Check Strict Superset/Check Strict Superset.py
+++ 04-Sets/13-Check Strict Superset/Check Strict Superset.py
@@ -1,15 +1,15 @@
 #https://www.hackerrank.com/challenges/py-check-strict-superset/problem?isFullScreen=true
 
-x=set(input().split())
-xd=0
-lol=int(input())
-for i in range(lol):
+set_x=set(input().split())
+count=0
+num=int(input())
+for i in range(num):
     ct=0
-    y= set(input().split())
-    if len(x)>len(y):
-        for z in y:
-            if z in x:
+    set_y= set(input().split())
+    if len(set_x)>len(set_y):
+        for z in set_y:
+            if z in set_x:
                 ct+=1
-        if ct==len(y):
-            xd+=1
-print("True" if xd==lol else "False")
+        if ct==len(set_y):
+            count+=1
+print("True" if count==num else "False")
