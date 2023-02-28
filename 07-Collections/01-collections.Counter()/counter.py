ddf1c05e6d1d858975245ce1b734eec40d04d7ab
lukezzz <patalee2001@gmail.com>
2023-02-28 09:53:28 +0530

    added

---
+++ 07-Collections/01-collections.Counter()/counter.py
@@ -0,0 +1,17 @@
+#https://www.hackerrank.com/challenges/collections-counter/problem?isFullScreen=true
+
+
+from collections import Counter
+
+x=int(input())
+y=Counter(map(int,input().split()))
+
+
+income=0
+for i in range(int(input())):
+  size,price=map(int,input().split())
+  if size in y.keys():
+    if y[size]!=0:
+      income+=price
+      y[size]-=1
+print(income)
