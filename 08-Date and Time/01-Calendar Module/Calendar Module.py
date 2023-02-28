0ef56a19b176667510dad9de9e5c0cf6a6becf43
lukezzz <patalee2001@gmail.com>
2023-02-28 11:22:02 +0530

    added

---
+++ 08-Date and Time/01-Calendar Module/Calendar Module.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/calendar-module/problem
+
+
+m,d,y=list(map(int,input().split()))
+import calendar
+wd=calendar.weekday(y,m,d)
+week=['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+print(week[wd])
---
+++ 08-Date and Time/02-Time Delta/TimeDelta.py
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/challenges/python-time-delta/problem?isFullScreen=true
+
+
+from datetime import datetime as dt
+
+
+fmt = '%a %d %b %Y %H:%M:%S %z'
+for i in range(int(input())):
+    print(int(abs((dt.strptime(input(), fmt) - 
+                   dt.strptime(input(), fmt)).total_seconds())))
