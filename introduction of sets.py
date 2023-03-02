a27979e138a31b58b8d4c6e542cfe373a39a0cf2
lukezzz <patalee2001@gmail.com>
2023-03-02 19:27:16 +0530

    removed

--- introduction of sets.py
+++
@@ -1,8 +0,0 @@
-
-def average(array):
-    return ("%.3f" %(sum(set(array))/len(set(array))))
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))
-    result = average(arr)
-    print(result)
--- weekday.py
+++
@@ -1,6 +0,0 @@
-
-m,d,y=list(map(int,input().split()))
-import calendar
-wd=calendar.weekday(y,m,d)
-week=['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
-print(week[wd])
