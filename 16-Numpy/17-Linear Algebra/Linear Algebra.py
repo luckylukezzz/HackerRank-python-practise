c92bddd5c37da31a33349d295f54f892dc5edf74
luckylukezzz <patalee2001@gmail.com>
2023-03-11 15:12:25 +0530

    added

--- 16-Numpy/17-Linear Algebra/Linear Algebra.py
+++
@@ -1,6 +0,0 @@
-#https://www.hackerrank.com/challenges/np-linear-algebra/problem?isFullScreen=true
-import numpy
-n=int(input())
-x=numpy.array([input().split() for i in range(n)],float)
-print(round(numpy.linalg.det(x),2))
-
